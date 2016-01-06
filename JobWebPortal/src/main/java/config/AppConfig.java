7ff034399f5c0323b13a753d5132a38310b8c769
roshanmutha <rcmutha@gmail.com>
2016-01-06 17:43:12 +0530

    Changes in configuration and added hibernate dependency

--- JobWebPortal/src/main/java/config/AppConfig.java
+++ JobWebPortal/src/main/java/config/AppConfig.java
@@ -2,9 +2,15 @@ package config;
 
 import java.util.Properties;
 
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp.BasicDataSource;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate4.LocalSessionFactoryBuilder;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -54,5 +60,32 @@ public class AppConfig extends WebMvcConfigurerAdapter {
 		viewResolver.setPrefix("/WEB-INF/views/");
 		viewResolver.setSuffix(".jsp");		
 		return viewResolver;
+	}
+	
+	@Bean(name = "dataSource")
+	public DataSource getDataSource() {
+	    BasicDataSource dataSource = new BasicDataSource();
+	    dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+	    dataSource.setUrl("jdbc:mysql://172.27.232.191:3306/jobportal");
+	    dataSource.setUsername("webuser");
+	    dataSource.setPassword("webpass");
+	    return dataSource;
+	}
+	
+	private Properties getHibernateProperties() {
+	    Properties properties = new Properties();
+	    properties.put("hibernate.show_sql", "true");
+	    properties.put("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+	    return properties;
+	}
+	
+	@Autowired
+	@Bean(name = "sessionFactory")
+	public SessionFactory getSessionFactory(DataSource dataSource) {
+	 
+	    LocalSessionFactoryBuilder sessionBuilder = new LocalSessionFactoryBuilder(dataSource);
+	    //sessionBuilder.addAnnotatedClasses(User.class);
+	    sessionBuilder.addProperties(getHibernateProperties());
+	    return sessionBuilder.buildSessionFactory();
 	}
 }
